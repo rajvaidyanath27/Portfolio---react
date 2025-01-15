b2e052f3a28cf80399704d5ee86c71c5e66e4cd0
Raj <rajbaidyanath76@gmail.com>
2025-01-16 05:14:23 +0530

    h

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants";
-import profilePic from "../assets/rajprofile.jpg";
+import profilePic from "../assets/kevinRushProfile.png";
 
 const Hero = () => {
     return (
