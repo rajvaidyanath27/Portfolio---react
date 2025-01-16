623d4d35cae6c7c77e4ecaf3fe8503afb14be5cb
Raj <rajbaidyanath76@gmail.com>
2025-01-17 00:35:30 +0530

    day-3

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,14 +1,111 @@
-import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
+import Swal from 'sweetalert2'
 
 const Contacts = () => {
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "b0db9507-f7c1-44d6-96b3-9bf577922f18");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      Swal.fire({
+        title: "Success!",
+        text: "Message Sent!",
+        icon: "success"
+      });
+    }
+  };
+
   return (
     <div className="border-b border-neutral-900 pb-20">
-        <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
-        <div className="text-center tracking-tighter">
-            <p className="my-4">{CONTACT.address}</p>
-            <p className="my-4">{CONTACT.phoneNo}</p>
-            <a href="#" className="border-b">{CONTACT.email}</a>
-        </div>
+        <motion.h1
+         whileInView={{opacity:1, y:0}}
+         initial={{opacity:0, y:-100}}
+         transition={{duration:1}}
+         className="my-10 text-center text-4xl">Get in Touch</motion.h1>
+        <motion.h3
+         whileInView={{opacity:1, y:0}}
+         initial={{opacity:0, y:-100}}
+         transition={{duration:1.5}}
+         className="text-center font-light bg-gradient-to-r from-pink-500 via-slate-700 to-purple-900 bg-clip-text text-2xl tracking-tight text-transparent">Send me a message and lets schedule a call</motion.h3>
+
+<motion.form onSubmit={onSubmit}
+ whileInView={{opacity:1, y:0}}
+ initial={{opacity:0, y:100}}
+ transition={{duration:1.5}}
+ className="bg-gradient-to-b from-purple-700  to-orange-800 p-6 rounded-2xl shadow-md  mt-10 max-w-md mx-auto">
+  <div className="input-box mb-4">
+    <input
+      type="text"
+      className="field w-full p-3 border text-black border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+      placeholder="First Name"
+      required
+    />
+  </div>
+
+  <div className="input-box mb-4">
+    <input
+      type="text"
+      className="field w-full p-3 border text-black border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+      placeholder="Last Name"
+      required
+    />
+  </div>
+
+  <div className="input-box mb-4">
+    <input
+      type="email"
+      className="field w-full p-3  border-gray-300 rounded-2xl text-black focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+      placeholder="Email"
+      required
+    />
+  </div>
+
+  <div className="input-box mb-4">
+    <input
+      type="text"
+      className="field w-full p-3 border text-black border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+      placeholder="Phone"
+      required
+    />
+  </div>
+
+  <div className="input-box mb-4">
+    <textarea
+      className="field w-full p-3 border border-gray-300 text-black rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+      placeholder="Your Message"
+      rows="4"
+      required
+    ></textarea>
+  </div>
+
+  <div className="flex justify-center">
+  <button
+    type="submit"
+    className="w-60 bg-orange-700 text-white py-3  rounded-2xl hover:bg-orange-600 transition duration-200"
+  >
+    Send Message
+  </button>
+  </div>
+
+</motion.form>
+
+
+        
       
     </div>
   )
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,36 +5,91 @@ import { FaNodeJs } from "react-icons/fa";
 import { SiMysql } from "react-icons/si";
 import { TbBrandFramerMotion } from "react-icons/tb";
 import { FaFigma } from "react-icons/fa";
+import { motion } from "framer-motion";
 
+const iconVariants = (duration) => ({
+   initial: { y: -10},
+   animate: {
+      y: [20, -20],
+      transition: {
+         duration: duration,
+         ease: "linear",
+         repeat: Infinity,
+         repeatType: 'reverse',
+      }
+   }
+})
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <h1 className="my-20 text-center text-4xl ">Technologies</h1>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">  
+        <motion.h1
+        whileInView={{ opacity :1,y:0}}
+        initial={{ opacity: 0,y: -100}}
+        tranistion={{duration:2}}
+         className="my-20 text-center text-4xl ">Technologies</motion.h1>
+        <motion.div
+        whileInView={{opacity: 1,x:0}}
+        initial={{opacity: 0,x: -100}}
+        tranistion={{duration: 1.5}}
+         className="flex flex-wrap items-center justify-center gap-4">
+            <motion.div
+            variants={iconVariants(1)}
+            initial="initial"
+            animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">  
                <DiMongodb className="text-5xl text-green-800"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">  
+            </motion.div>
+
+            <motion.div
+             variants={iconVariants(1.3)}
+             initial="initial"
+             animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">  
                <SiExpress className="text-5xl text-green-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">  
+            </motion.div>
+
+            <motion.div
+             variants={iconVariants(1.6)}
+             initial="initial"
+             animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">  
                <FaReact className="text-5xl text-cyan-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">  
+            </motion.div>
+
+            <motion.div
+             variants={iconVariants(1.9)}
+             initial="initial"
+             animate="animate" 
+            className="rounded-2xl border-4 border-neutral-800 p-4">  
                <FaNodeJs className="text-5xl text-green-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">  
+            </motion.div>
+
+            <motion.div
+             variants={iconVariants(2.3)}
+             initial="initial"
+             animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">  
                <SiMysql className="text-5xl text-blue-800"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">  
+            </motion.div>
+
+            <motion.div
+             variants={iconVariants(2.6)}
+             initial="initial"
+             animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">  
                <TbBrandFramerMotion className="text-5xl text-red-800"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">  
+            </motion.div>
+
+            <motion.div
+             variants={iconVariants(2.8)}
+             initial="initial"
+             animate="animate"
+             className="rounded-2xl border-4 border-neutral-800 p-4">  
                <FaFigma className="text-5xl text-pink-600"/>
-            </div>
+            </motion.div>
             
-        </div>
+        </motion.div>
     </div>
   );
 };
