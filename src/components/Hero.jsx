dcf0067e916b82b8a534aec8bfb39886fdefddaa
Raj <rajbaidyanath76@gmail.com>
2025-05-12 18:31:29 +0530

    up

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
             >
-              MERN Stack Developer
+              Full Stack Developer
             </motion.span>
             
             <a href="https://pixahive.com/photo/get-lost-illustration/" target="_blank" rel="noopener noreferrer">
